ea4646d54220c2b8ec62818c160b143bb78d846a
Subham Singh <subham0506singh@gmail.com>
2025-09-18 00:27:08 +0530

    delete auth2.tsx

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx"),
-    route('/auth','routes/auth.tsx'),
+    route('/auth','routes/auth2.tsx'),
     route('/upload','routes/upload.tsx'),
     route('/resume/:id','routes/resume.tsx'),
      route('/wipe', 'routes/wipe.tsx'),
